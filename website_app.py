3c1f6ad1a3d8fa340185356c32ee0e04e8060879
Wibnu Dickson <wibnudickson@gmail.com>
2021-08-17 01:04:43 +0200

    basic webapage

--- website_app.py
+++ website_app.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Home goes here!"
+
+@app.route('/about/')
+def about():
+    return "Webiste content goes here!"
+
+if __name__=="__main__":
+    app.run(debug=True)
 
